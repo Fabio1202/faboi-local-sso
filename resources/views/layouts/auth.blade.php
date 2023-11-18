2f3e5d664d2672b1b4c8e783322049c8deafaa03
Fabio Boi <fabio.boi@icloud.com>
2023-11-18 01:43:59 +0100

    Small changes for logo

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,9 +2,11 @@
     <div class="flex w-screen">
         <div class="w-screen overflow-hidden bg-white min-h-screen md:w-2/5 md:px-12 dark:bg-gray-800 dark:text-white">
             <div class="mt-10 w-full flex justify-center flex-wrap">
-                <x-application-logo class="w-20 h-20 fill-current text-primary dark:text-white"/>
+                <div class="w-52">
+                    <x-application-logo/>
+                </div>
             </div>
-            <h1 class="font-bold text-6xl px-4 md:px-0 pb-5 pt-10">{{ $title }}</h1>
+            <h1 class="font-bold text-6xl px-4 md:px-0 pb-5 pt-14">{{ $title }}</h1>
             {{ $slot }}
         </div>
         <div class="hidden md:flex w-3/5 h-screen justify-center items-center content-center bg-gray-100 dark:bg-gray-900">
