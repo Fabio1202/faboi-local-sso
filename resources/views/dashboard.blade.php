591bed7312e6da9ff5da3a79af14229f6ec5d036
Fabio Boi <fabio.boi@icloud.com>
2025-02-11 13:59:06 +0100

    Run Upgrade Script

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <button id="passkey-start-registration" class="mt-6 block w-full py-3 border-solid border-2 border-gray-300 rounded-md text-xl">
                         <i class="fa-solid fa-fingerprint mr-3"></i>
