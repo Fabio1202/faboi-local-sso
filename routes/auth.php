0ef566d412b00d6c2367d67527e0fcc9c51c4b88
Fabio <fabio.boi@icloud.com>
2025-07-13 22:22:13 +0200

    Added userinfo

--- routes/auth.php
+++ routes/auth.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware(['guest:'.config('fortify.guard')])
     ->name('password.email');
+
+Route::get('/oauth/userinfo', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api')->name('openid.userinfo');
