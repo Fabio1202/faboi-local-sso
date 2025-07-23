51ec64abee3bc69a8095795c6e2a075046cedc05
Fabio <fabio.boi@icloud.com>
2025-07-24 00:09:39 +0200

    Some changes to code

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -5,21 +5,31 @@ namespace App\Http\Requests;
 use App\Models\Application;
 use App\Models\Client;
 use Illuminate\Http\Request as BaseRequest;
+use Laravel\Passport\Passport;
 use Laravel\Passport\Token;
 use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Token\Plain;
 
 class ClientRequest extends BaseRequest
 {
     public function client(): ?Client
     {
-        $bearerToken = request()->bearerToken();
-        /** @noinspection PhpUndefinedMethodInspection */
-        // @phpstan-ignore-next-line
-        $tokenId = Configuration::forUnsecuredSigner()->parser()->parse($bearerToken)->claims()->get('jti');
+        // todo: implement this function to return the client based on the request
+        return null;
 
-        /** @var Client */
-        // @phpstan-ignore-next-line
-        return Token::find($tokenId)->client;
+//        $bearerToken = request()->bearerToken();
+//
+//        /** @phpstan-ignore-next-line */
+//        $tokenId = Configuration::forAsymmetricSigner(
+//            new Sha256(),
+//            InMemory::plainText(config('passport.private_key')),
+//            InMemory::file(config('passport.public_key'))
+//        )->parser()->parse($bearerToken)->claims()->get('jti');
+//
+//        /** @phpstan-ignore-next-line */
+//        return Token::find($tokenId)->client;
     }
 
     public function application(): Application
